a246f2f79f8aeab5ae4c064270a7e21686f75f09
Dandush03 <d.laloush@outlook.com>
2020-03-30 11:33:09 -0400

    added menu function

--- app/controllers/cultures_controller.rb
+++ app/controllers/cultures_controller.rb
@@ -1,19 +1,29 @@
 class CulturesController < ApplicationController
+  
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    follower_list = @user.followeds.select(:follower_id)
-    @fallowed_by ||= User.all.where(id: follower_list)
+    follower_list = @user.followeds.select(:follower_id).limit(3)
+    @followed_by ||= User.all.where(id: follower_list)
+    @partial ||= sub_menu(params[:test])
   end
-
-  def new; end
 
   def create; end
 
   def update; end
 
   def delete; end
+  
+  private
+
+  def sub_menu(id = nil)
+    case id
+    when nil then 'user_list'
+    when "1" then 'cover'
+    else 'tesr'
+    end
+	end
 end
