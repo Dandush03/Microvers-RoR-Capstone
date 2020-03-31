b556ce5977f1bea5e6683f0c6ca121ab3288ff34
Dandush03 <d.laloush@outlook.com>
2020-03-31 15:39:33 -0400

    Finishing adjusting minors bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    att_create = %i[full_name username email password password_confirmation]
+    att_create = %i[profile_img full_name username email password password_confirmation]
     att_update = %i[cover_img profile_img full_name username email password current_password]
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(att_create) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(att_update) }
