3e89c8b9e53dca405323e9eb4bbb78a891abdf9f
Dandush03 <d.laloush@outlook.com>
2020-03-31 16:57:16 -0400

    fixed minors bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,6 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for(resource)
-    culture_path(resource)
-  end
-
-  def user_root_path
-    culture_path(current_user)
-  end
-
   def configure_permitted_parameters
     att_create = %i[profile_img full_name username email password password_confirmation]
     att_update = %i[cover_img profile_img full_name username email password current_password]
